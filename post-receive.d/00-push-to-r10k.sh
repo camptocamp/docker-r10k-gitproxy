0487b9ef19589f41fecfd199a900444c29ef20ae
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-12 13:09:22 +0100

    r10k mcollective agent always implies -p

--- post-receive.d/00-push-to-r10k.sh
+++ post-receive.d/00-push-to-r10k.sh
@@ -5,19 +5,7 @@ while read oldrev newrev refname; do
   # R10K
   if [[ $refname =~ 'refs/heads/' ]]; then
     branch=$(git rev-parse --symbolic --abbrev-ref $refname)
-    if [[ $newrev = '0000000000000000000000000000000000000000' ]]; then
-      echo "r10k removing $branch environment"
-      mco r10k deploy environment $branch
-    else
-      files=$(git diff-tree -r --name-only --no-commit-id ${oldrev}..${newrev})
-      if grep -q 'Puppetfile' <<<"$files"; then
-        echo "r10k updating $branch environment and modules"
-        mco r10k deploy environment $branch -p
-      else
-        echo "r10k updating $branch environment"
-        mco r10k deploy environment $branch
-      fi
-    fi
+    mco r10k deploy $branch
   else
     echo "r10k skipping $refname"
   fi
