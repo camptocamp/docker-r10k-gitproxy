4dab46f1c8ec1caade965897a3f23b11089fa265
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-15 16:29:32 +0100

    Fix id_rsa temporary location

--- docker-entrypoint.d/05-generate-authorized_keys.sh
+++ docker-entrypoint.d/05-generate-authorized_keys.sh
@@ -3,7 +3,7 @@
 if test -n "${RSA_PRIVATE_KEY}"; then
   echo -e $(echo $RSA_PRIVATE_KEY | sed -e 's/ /\\n/g' \
     -e 's/-----BEGIN\\nRSA\\nPRIVATE\\nKEY-----/-----BEGIN RSA PRIVATE KEY-----/' \
-    -e 's/-----END\\nRSA\\nPRIVATE\\nKEY-----/-----END RSA PRIVATE KEY-----/') > /root/.ssh/id_rsa
-  chmod 0600 /root/.ssh/id_rsa
-  ssh-keygen -y -f /root/.ssh/id_rsa >> /home/r10k/.ssh/authorized_keys
+    -e 's/-----END\\nRSA\\nPRIVATE\\nKEY-----/-----END RSA PRIVATE KEY-----/') > /tmp/id_rsa
+  chmod 0600 /tmp/id_rsa
+  ssh-keygen -y -f /tmp/id_rsa >> /home/r10k/.ssh/authorized_keys
 fi
