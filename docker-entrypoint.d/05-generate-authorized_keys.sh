0ee9955ddd580e8fe5ee1d373dd406e4af556f90
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-17 17:34:45 +0100

    Fix private key creation

--- docker-entrypoint.d/05-generate-authorized_keys.sh
+++ docker-entrypoint.d/05-generate-authorized_keys.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if test -n "${RSA_PRIVATE_KEY}"; then
-  echo -e $(echo $RSA_PRIVATE_KEY | sed -e 's/ /\\n/g' \
-    -e 's/-----BEGIN\\nRSA\\nPRIVATE\\nKEY-----/-----BEGIN RSA PRIVATE KEY-----/' \
-    -e 's/-----END\\nRSA\\nPRIVATE\\nKEY-----/-----END RSA PRIVATE KEY-----/') > /tmp/id_rsa
+  echo -e "${RSA_PRIVATE_KEY}" > /tmp/id_rsa
   chmod 0600 /tmp/id_rsa
   ssh-keygen -y -f /tmp/id_rsa >> /home/r10k/.ssh/authorized_keys
   rm -f /tmp/id_rsa
