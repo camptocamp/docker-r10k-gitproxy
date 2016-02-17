dbab2238b2f0436cfa064abd29bd07be2e10284c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-17 11:07:52 +0100

    Revert "Import pubkeys from puppet ca"
    This reverts commit 7b69e6ee5fbb7498b8d09865a9bd64739e264c86.

--- docker-entrypoint.d/05-generate-authorized_keys.sh
+++ docker-entrypoint.d/05-generate-authorized_keys.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if getent hosts rancher-metadata > /dev/null ; then
-  curl http://rancher-metadata/latest/self/stack/services/r10k/containers | while read container; do
-    uuid=$(curl http://rancher-metadata/latest/self/stack/services/r10k/containers/$(echo container|cut -f1 -d'=')/uuid)
-    curl --cert /etc/puppetlabs/puppet/ssl/certs/$(hostname -f).pem \
-      --key /etc/puppetlabs/puppet/ssl/private_keys/$(hostname -f).pem \
-      --cacert /etc/puppetlabs/puppet/ssl/ca/ca_crt.pem \
-      https://puppetca:8140/puppet-ca/v1/certificate/$uuid?environment=production > /tmp/$uuid.pem
-    openssl x509 -pubkey -noout -in $uuid.pem  >> /home/r10k/.ssh/authorized_keys
-  done
+if test -n "${RSA_PRIVATE_KEY}"; then
+  echo -e $(echo $RSA_PRIVATE_KEY | sed -e 's/ /\\n/g' \
+    -e 's/-----BEGIN\\nRSA\\nPRIVATE\\nKEY-----/-----BEGIN RSA PRIVATE KEY-----/' \
+    -e 's/-----END\\nRSA\\nPRIVATE\\nKEY-----/-----END RSA PRIVATE KEY-----/') > /tmp/id_rsa
+  chmod 0600 /tmp/id_rsa
+  ssh-keygen -y -f /tmp/id_rsa >> /home/r10k/.ssh/authorized_keys
+  rm -f /tmp/id_rsa
 fi
