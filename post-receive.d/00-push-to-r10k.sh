a39ec79cfe1422945d035ae69e1969aef646363b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-19 13:26:34 +0100

    Get refname...

--- post-receive.d/00-push-to-r10k.sh
+++ post-receive.d/00-push-to-r10k.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-TMPFILE=$(mktemp --suffix=.json)
-
-cat << EOF > $TMPFILE
-{
-  "ref": "${refname}"
-}
+while read oldrev newrev refname; do
+  tmpfile=$(mktemp --suffix=.json)
+  
+  cat << EOF > $tmpfile
+  {
+    "ref": "${refname}"
+  }
 EOF
-
-SIG=$(cat "${TMPFILE}" | openssl dgst -sha1 -hmac "${WEBHOOK_SECRET}" | awk '{print "X-Hub-Signature: sha1="$2}')
-
-curl -H "Content-Type: application/json" -H "${SIG}" --data "@${TMPFILE}" http://webhook:9000/hooks/r10k
-
-rm -f "${TMPFILE}"
+  
+  sig=$(cat "${tmpfile}" | openssl dgst -sha1 -hmac "${WEBHOOK_SECRET}" | awk '{print "X-Hub-Signature: sha1="$2}')
+  
+  curl -H "Content-Type: application/json" -H "${sig}" --data "@${tmpfile}" http://webhook:9000/hooks/r10k
+  
+  rm -f "${tmpfile}"
+done
