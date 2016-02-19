2ca3afe48b8efb5cef728af4dfdfa75ff00cbde9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-19 13:46:45 +0100

    Use data-urlencode and payload@

--- post-receive.d/00-push-to-r10k.sh
+++ post-receive.d/00-push-to-r10k.sh
@@ -11,7 +11,7 @@ EOF
   
   sig=$(cat "${tmpfile}" | openssl dgst -sha1 -hmac "${WEBHOOK_SECRET}" | awk '{print "X-Hub-Signature: sha1="$2}')
   
-  curl -H "Content-Type: application/json" -H "${sig}" --data "@${tmpfile}" http://webhook:9000/hooks/r10k
+  curl -H "Content-Type: application/json" -H "${sig}" --data-urlencode "payload@${tmpfile}" http://webhook:9000/hooks/r10k
   
   rm -f "${tmpfile}"
 done
