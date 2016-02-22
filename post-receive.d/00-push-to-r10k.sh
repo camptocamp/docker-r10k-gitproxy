cf62ce9bbd4a69c63484dc2ea28b82f10e2d92cc
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-22 13:50:32 +0100

    Fix sha1 generation

--- post-receive.d/00-push-to-r10k.sh
+++ post-receive.d/00-push-to-r10k.sh
@@ -2,16 +2,12 @@
 
 while read oldrev newrev refname; do
   tmpfile=$(mktemp --suffix=.json)
+
+  data="{\"ref\": \"${refname}\"}"
   
-  cat << EOF > $tmpfile
-  {
-    "ref": "${refname}"
-  }
-EOF
+  sig=$(echo -n "${data}" | openssl dgst -sha1 -hmac "${WEBHOOK_SECRET}" | awk '{print "X-Hub-Signature: sha1="$2}')
   
-  sig=$(cat "${tmpfile}" | openssl dgst -sha1 -hmac "${WEBHOOK_SECRET}" | awk '{print "X-Hub-Signature: sha1="$2}')
-  
-  curl -X POST -H "Content-Type: application/json" -H "${sig}" --data-urlencode "payload@${tmpfile}" http://webhook:9000/hooks/r10k
+  curl -X POST -H "Content-Type: application/json" -H "${sig}" --data "${data}" http://webhook:9000/hooks/r10k
   
   rm -f "${tmpfile}"
 done
